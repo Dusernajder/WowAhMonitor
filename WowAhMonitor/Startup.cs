52934b0aeea634c510c887eac2daf3e1c89a38e9
Dusernajder <dusungulu.tibi@gmail.com>
2021-04-15 09:51:24 +0200

    appsettings DI setup

--- WowAhMonitor/Startup.cs
+++ WowAhMonitor/Startup.cs
@@ -1,23 +1,33 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Settings;
 using WowAhMonitor.Services;
 
 namespace WowAhMonitor
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.Configure<BlizzardApiSettings>(Configuration.GetSection("BlizzardApiSettings"));
             services.AddScoped<IBlizzardApiService, BlizzardApiService>();
             services.AddScoped<IBlizzardOAuth2Service, BlizzardOAuth2Service>();
-            services.AddTransient<IBlizzardOAuth2Service, BlizzardOAuth2Service>();
-            services.AddHttpClient();
             services.AddRazorPages();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
