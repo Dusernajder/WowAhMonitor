988bd455f2a897b988433babdefa987466d00ff7
Dusernajder <dusungulu.tibi@gmail.com>
2021-04-29 11:34:01 +0200

    Converted to Singleton, implemented IsTokenValid method, GetToken method returns token string

--- WowAhMonitor/Services/BlizzardOAuth2Service.cs
+++ WowAhMonitor/Services/BlizzardOAuth2Service.cs
@@ -15,6 +15,7 @@ namespace WowAhMonitor.Services
     {
         private readonly IHttpClientFactory _clientFactory;
         private readonly BlizzardApiSettings _blizzardApiSettings;
+        private AccessTokenCredentialsResponse _tokenCredentialsResponse;
 
         public BlizzardOAuth2Service(IHttpClientFactory clientFactory, IOptions<BlizzardApiSettings> settings)
         {
@@ -22,31 +23,42 @@ namespace WowAhMonitor.Services
             _blizzardApiSettings = settings.Value;
         }
 
-        public async Task<AccessTokenCredentialsResponse> GetToken()
+        public async Task<string> GetToken()
         {
-            var uri = String.Format(_blizzardApiSettings.Links.AccessTokenUrl, "eu");
-            using var client = _clientFactory.CreateClient();
-            var request = new HttpRequestMessage(new HttpMethod("POST"), uri);
-            var base64Authorization =
-                Convert.ToBase64String(Encoding.ASCII.GetBytes(
-                    $"{_blizzardApiSettings.ClientInfo.ClientId}:{_blizzardApiSettings.ClientInfo.ClientSecret}")
-                );
+            if (_tokenCredentialsResponse == null || !IsTokenValid())
+            {
+                var uri = String.Format(_blizzardApiSettings.Links.AccessTokenUrl, "eu");
+                using var client = _clientFactory.CreateClient();
+                var request = new HttpRequestMessage(new HttpMethod("POST"), uri);
+                var base64Authorization =
+                    Convert.ToBase64String(Encoding.ASCII.GetBytes(
+                        $"{_blizzardApiSettings.ClientInfo.ClientId}:{_blizzardApiSettings.ClientInfo.ClientSecret}")
+                    );
 
-            request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64Authorization}");
-            request.Content = new StringContent("grant_type=client_credentials");
-            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64Authorization}");
+                request.Content = new StringContent("grant_type=client_credentials");
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
 
-            var response = await client.SendAsync(request);
-            var textResult = await response.Content.ReadAsStringAsync();
-            var myObjResult = JsonConvert.DeserializeObject<AccessTokenCredentialsResponse>(textResult);
+                var response = await client.SendAsync(request);
+                var textResult = await response.Content.ReadAsStringAsync();
+                _tokenCredentialsResponse = JsonConvert.DeserializeObject<AccessTokenCredentialsResponse>(textResult);
+            }
 
-
-            return myObjResult;
+            return _tokenCredentialsResponse.AccessToken;
         }
 
         public bool IsTokenValid()
         {
-            throw new System.NotImplementedException();
+            var tokenTimeStamp = _tokenCredentialsResponse.ExpiresIn;
+            DateTime currentTime = DateTime.Now;
+            var tokenTimeExpires = currentTime.AddSeconds(tokenTimeStamp).ToLocalTime();
+            if (currentTime < tokenTimeExpires)
+            {
+                Console.WriteLine($"Expires at: {tokenTimeExpires}");
+                return true;
+            }
+
+            return false;
         }
     }
 }
